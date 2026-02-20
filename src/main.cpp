2b6b6cd8ad7595bea019eba7e8f2b61ba8131b88
Terra <terra@debian.lan>
2026-02-20 12:52:43 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -31,13 +31,14 @@ std::string Shell::eval(){
       return command + ": command not found\n";
   }
 }
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  Shell shell = Shell();
+  shell::Shell shell{};
   shell.run();
  }
-}
+
